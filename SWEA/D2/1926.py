446abfc09d71d37cf3062b791966de7231dc5af1
snowwhitewolf <whdgus1029@naver.com>
2021-08-11 18:23:58 +0900

    0811

--- SWEA/D2/1926.py
+++ SWEA/D2/1926.py
@@ -1,10 +1,7 @@
-a = list(range(int(input())+1))
-
-for i in range(1,len(a)):
-    if '3' in str(i):
-        a[i] = '-'
-    elif '6' in str(i):
-        a[i] = '-'
-    elif '9' in str(i):
-        a[i] = '-'
-print(a)
+cnt = [0 for _ in range(int(input()))]
+for i in range(1,len(cnt)+1):
+    cnt[i-1] += str(i).count('3') + str(i).count('6') + str(i).count('9')
+    if cnt[i-1] == 0:
+        print(i, end = ' ')
+    else:
+        print('-'*cnt[i-1], end = ' ')
