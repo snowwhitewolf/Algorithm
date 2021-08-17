c8392a4bae1db572525edfc9ce69e7a2a11d8a6e
snowwhitewolf <whdgus1029@naver.com>
2021-08-17 22:06:27 +0900

    0817

--- SWEA/D3/_10965. 제곱수 만들기.py
+++ SWEA/D3/_10965. 제곱수 만들기.py
@@ -1,12 +1,27 @@
-b = []
+num = [1]*(10**7+1)
+num[0] = 0
+num[1] = 0
+for i in range(2,(int((10**7+1)**0.5)+1)):
+    if num[i] == 1:
+        j = 2
+        while i*j <= 10**7:
+            num[i*j] = 0
+            j+=1
+            
+result = []
 for t in range(int(input())):
     a = int(input())
-    cnt = 0
-    while cnt == 0:
-        for j in range(1,a+1):
-            if ((a*j)**0.5)%1 == 0 and int((a*j)**0.5)**2 == a*j:
-                b.append(j)
-                cnt = 1
-                break
-for i in range(1,len(b)+1):
-    print('#{} {}'.format(i,b[i-1]))
+    i = 2
+    b=1
+    while a > 3164:
+        cnt = 0
+        if num[i]:
+            while a%i == 0:
+                a = a//i
+                cnt += 1
+            if cnt%2 == 1:
+                b *= i
+        i += 1
+    result.append(b)
+for c in range(len(result)):
+    print('#{} {}'.format(c+1,result[c]))
