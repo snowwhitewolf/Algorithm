bb24a371845203504f01c287829245abaa20aecf
snowwhitewolf <whdgus1029@naver.com>
2021-09-11 18:45:51 +0900

    0911

---
+++ Baekjoon/4673.py
@@ -0,0 +1,11 @@
+lst = list(i for i in range(10001))
+result = list(i for i in range(1,10001))
+i=1
+for i in range(10001):
+    n = lst[i]
+    for j in range(len(str(lst[i]))):
+        n += int(str(lst[i])[j])
+    if n in result and n < 10001:
+        result.remove(n)
+for i in range(len(result)):
+    print(result[i])
