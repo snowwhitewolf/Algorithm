c7772d108cc5a0026bc5f63676903fb8c98fe0a5
snowwhitewolf <whdgus1029@naver.com>
2021-09-14 22:03:45 +0900

    0914

---
+++ Baekjoon/14889.py
@@ -0,0 +1,25 @@
+from itertools import combinations
+N = int(input())
+lst = [list(map(int,input().split())) for _ in range(N)]
+
+def score(lst_1,lst_2):
+    team1 = 0
+    team2 = 0
+    for i in lst_1:
+        for j in lst_1:
+            team1 += lst[i-1][j-1]
+    for i in lst_2:
+        for j in lst_2:
+            team2 += lst[i-1][j-1]
+    return abs(team1-team2)
+res = 100000000
+
+team_2 = []
+team = list(combinations(list(range(1,N+1)),N//2))
+for q in range(len(team)):
+    team_1 = []
+    for w in team[q]:
+        team_1.append(w)
+    team_2 = [k for k in range(1,N+1) if k not in team_1]
+    res = min(res, score(team_1, team_2))
+print(res)
