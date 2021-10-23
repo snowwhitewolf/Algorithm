84034475e44d5f112a9e00493d5c8b52fd592b43
snowwhitewolf <whdgus1029@naver.com>
2021-10-23 12:36:31 +0900

    1023

---
+++ Baekjoon/10430.py
@@ -0,0 +1,9 @@
+a,b,c, = map(int,input().split())
+
+res = []
+res.append((a+b)%c)
+res.append(((a%c) + (b%c))%c)
+res.append((a*b)%c)
+res.append(((a%c) * (b%c))%c)
+for i in range(len(res)):
+    print(res[i])
