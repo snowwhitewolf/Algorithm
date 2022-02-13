ed9b4322159dbff11e03f088eb424b79d9636387
snowwhitewolf <whdgus1029@naver.com>
2022-02-13 22:16:56 +0900

    0213

--- Baekjoon/1002.py
+++ Baekjoon/1002.py
@@ -1,5 +1,5 @@
 for t in range(int(input())):
-    x1,y1,r1,x2,y2,r2 = map(int,input().split())
+    x1, y1, r1, x2, y2, r2 = map(int, input().split())
     r = ((x1 - x2) ** 2 + (y1 - y2) ** 2)**(1/2)
     if r == 0 and r1 == r2:
         print(-1)
