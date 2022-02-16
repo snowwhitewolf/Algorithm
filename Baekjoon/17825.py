2f22fcf2c629a511f1c6dee96614a93136f70b5e
snowwhitewolf <whdgus1029@naver.com>
2022-02-16 23:02:30 +0900

    0216

--- Baekjoon/17825.py
+++ Baekjoon/17825.py
@@ -9,50 +9,51 @@ import itertools
 from itertools import product
 
 
-num = list(map(int,input().split()))
+num = list(map(int, input().split()))
 lst = [
-    [0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40],#반환점 없이 이동
-    [10,13,16,19,25,30,35,40],#10번에 갔을때
-    [20,22,24,25,30,35,40],#20번에 갔을때
-    [30,28,27,26,25,30,35,40]#30번에 갔을때
-    ]
+    [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24,
+        26, 28, 30, 32, 34, 36, 38, 40],  # 반환점 없이 이동
+    [10, 13, 16, 19, 25, 30, 35, 40],  # 10번에 갔을때
+    [20, 22, 24, 25, 30, 35, 40],  # 20번에 갔을때
+    [30, 28, 27, 26, 25, 30, 35, 40]  # 30번에 갔을때
+]
 visited = [
     [0]*len(lst[0]),
     [0]*len(lst[1]),
     [0]*len(lst[2]),
     [0]*len(lst[3]),
 ]
-idx = [0,0,0,0]#각 말의 위치
+idx = [0, 0, 0, 0]  # 각 말의 위치
 point = 0
 result = 0
-root = [0,0,0,0]#이동하고 있는 경로
-goal = [1,1,1,1]#골인 유무
+root = [0, 0, 0, 0]  # 이동하고 있는 경로
+goal = [1, 1, 1, 1]  # 골인 유무
 
-data = itertools.product([0,1,2,3], repeat = 10)
+data = itertools.product([0, 1, 2, 3], repeat=10)
 max_dis = [len(lst[0])-1]*4
 for dice in data:
     point = 0
     for j in range(10):
-        if goal[dice[j]]:#도착하지 않았을때
-            if max_dis[j] < idx[j]+num[j]:#이번 이동으로 골인한다면
+        if goal[dice[j]]:  # 도착하지 않았을때
+            if max_dis[j] < idx[j]+num[j]:  # 이번 이동으로 골인한다면
                 goal[dice[j]] = 0
-            elif visited[root[dice[j]]][idx[j]+num[j]] == 0:#중복이 아니면 이동
-                visited[root[dice[j]]][idx[j]] = 0#원래 있던 자리 초기화
-                idx[dice[j]] += num[j]#위치를 주사위 값만큼 이동
-                visited[root[dice[j]]][idx[dice[j]]] = 1#지금 있는 자리 추가
-                point += lst[root[dice[j]]][idx[dice[j]]]#점수 추가
-                if lst[idx[dice[j]]] == 10:#10번에 위치
+            elif visited[root[dice[j]]][idx[j]+num[j]] == 0:  # 중복이 아니면 이동
+                visited[root[dice[j]]][idx[j]] = 0  # 원래 있던 자리 초기화
+                idx[dice[j]] += num[j]  # 위치를 주사위 값만큼 이동
+                visited[root[dice[j]]][idx[dice[j]]] = 1  # 지금 있는 자리 추가
+                point += lst[root[dice[j]]][idx[dice[j]]]  # 점수 추가
+                if lst[idx[dice[j]]] == 10:  # 10번에 위치
                     idx[dice[j]] = 0
                     root[dice[j]] = 1
                     max_dis[dice[j]] = len(lst[1]-1)
-                elif lst[idx[dice[j]]] == 20:#20번에 위치
+                elif lst[idx[dice[j]]] == 20:  # 20번에 위치
                     idx[dice[j]] = 0
                     root[dice[j]] = 2
                     max_dis[dice[j]] = len(lst[2]-1)
-                elif lst[idx[dice[j]]] == 30:#30번에 위치
+                elif lst[idx[dice[j]]] == 30:  # 30번에 위치
                     idx[dice[j]] = 0
                     root[dice[j]] = 3
                     max_dis[dice[j]] = len(lst[3]-1)
-    result = max(result,point)
-    
+    result = max(result, point)
+
 print(result)
--- Baekjoon/21608.py
+++ Baekjoon/21608.py
@@ -1,7 +1,7 @@
 def first(man):
     total = []
     first_MAP = [[0]*N for _ in range(N)]
-    max_cnt=0
+    max_cnt = 0
     for y in range(N):
         for x in range(N):
             if MAP[y][x]:
@@ -13,13 +13,14 @@ def first(man):
                 if 0 <= bfy < N and 0 <= bfx < N:
                     if MAP[bfy][bfx] in like[f'{man}']:
                         cnt += 1
-            max_cnt = max(cnt,max_cnt)
+            max_cnt = max(cnt, max_cnt)
             first_MAP[y][x] = cnt
     for y in range(N):
         for x in range(N):
-            if first_MAP[y][x] == max_cnt and MAP[y][x]==0:
-                total.append([y,x])
+            if first_MAP[y][x] == max_cnt and MAP[y][x] == 0:
+                total.append([y, x])
     return total
+
 
 def second(lst):
     if len(lst) == 1:
@@ -34,12 +35,12 @@ def second(lst):
             bfy = y + dy[d]
             bfx = x + dx[d]
             if 0 <= bfy < N and 0 <= bfx < N:
-                if MAP[bfy][bfx]==0:
+                if MAP[bfy][bfx] == 0:
                     cnt += 1
         max_cnt = max(cnt, max_cnt)
         lst[i].append(cnt)
     for i in range(len(lst)):
-        if lst[i][2] == max_cnt and MAP[lst[i][0]][lst[i][1]]==0:
+        if lst[i][2] == max_cnt and MAP[lst[i][0]][lst[i][1]] == 0:
             second_MAP.append(lst[i][0:2])
     return second_MAP
 
@@ -48,11 +49,11 @@ N = int(input())
 like = {}
 lst = []
 MAP = [[0]*N for _ in range(N)]
-dy = [0,0,1,-1]
-dx = [1,-1,0,0]
+dy = [0, 0, 1, -1]
+dx = [1, -1, 0, 0]
 res = 0
 for _ in range(N*N):
-    a = list(map(int,input().split()))
+    a = list(map(int, input().split()))
     lst.append(a[0])
     like[f'{a[0]}'] = a[1:]
 MAP[1][1] = lst[0]
@@ -67,7 +68,7 @@ for y in range(N):
         for d in range(4):
             bfy = y+dy[d]
             bfx = x+dx[d]
-            if 0<= bfy < N and 0<= bfx < N:
+            if 0 <= bfy < N and 0 <= bfx < N:
                 if MAP[bfy][bfx] in like[f'{MAP[y][x]}']:
                     cnt += 1
         if cnt != 0:
