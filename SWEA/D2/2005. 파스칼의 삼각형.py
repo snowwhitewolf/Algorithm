8c7b6468f74a20b13793529caa80427152ac73b4
snowwhitewolf <whdgus1029@naver.com>
2022-02-26 23:08:48 +0900

    0226

--- SWEA/D2/2005. 파스칼의 삼각형.py
+++ SWEA/D2/2005. 파스칼의 삼각형.py
@@ -1,12 +1,12 @@
 for t in range(int(input())):
-    N=int(input())
+    N = int(input())
     result = []
-    for i in range(1,N+1):
+    for i in range(1, N+1):
         result.append([0]*i)
-    for i in range(0,N):
+    for i in range(0, N):
         result[i][0] = 1
-        result[i][-1] =1
-        for j in range(1,i):
+        result[i][-1] = 1
+        for j in range(1, i):
             result[i][j] += result[i-1][j-1] + result[i-1][j]
 
     print(f'#{t+1}')
--- SWEA/D2/2007. 패턴 마디의 길이.py
+++ SWEA/D2/2007. 패턴 마디의 길이.py
@@ -1,7 +1,7 @@
 for t in range(int(input())):
     a = str(input())
     result = 0
-    for i in range(2,len(a)//2):
+    for i in range(2, len(a)//2):
         if a[0:i] == a[i:i*2]:
             result = i
             break
