e172f010915bd2e8a710b4e1603b1edd5087bdd4
snowwhitewolf <whdgus1029@naver.com>
2022-02-28 21:52:59 +0900

    0228

--- Baekjoon/20055.py
+++ Baekjoon/20055.py
@@ -3,7 +3,7 @@ A = list(map(int, input().split()))
 robot = [0 for _ in range(2*N)]
 cnt = 0
 while True:
-    # 1번째 단계
+
     cnt += 1
     next = A[-1]
     del A[-1]
@@ -14,7 +14,6 @@ while True:
     if robot[N-1]:
         robot[N-1] -= 1
 
-    # 2번째 단계
     for i in range(N-2, -1, -1):
         if robot[i] and A[i+1] and robot[i+1] == 0:
             robot[i] -= 1
@@ -23,12 +22,10 @@ while True:
     if robot[N - 1]:
         robot[N - 1] = 0
 
-    # 3번째 단계
     if A[0] and robot[0] == 0:
         robot[0] = 1
         A[0] -= 1
 
-    # 4번째 단계
     if A.count(0) >= K:
         break
 
