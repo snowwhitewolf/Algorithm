e5a5e9847bd5be995e8e05ddbf386842d4313b7c
snowwhitewolf <whdgus1029@naver.com>
2022-03-06 22:27:55 +0900

    0306

--- Baekjoon/3190.py
+++ Baekjoon/3190.py
@@ -2,7 +2,7 @@ n = int(input())
 k = int(input())
 board = list([0]*n for _ in range(n))
 for _ in range(k):
-    y,x = map(int,input().split())
+    y, x = map(int, input().split())
     board[y-1][x-1] = 2
 
 l = int(input())
@@ -10,14 +10,14 @@ move = []
 for _ in range(l):
     move.append(list(input().split()))
 
-bam = [[0,0]]
+bam = [[0, 0]]
 time = 0
 eat = 0
-dy = [0,1,0,-1]# 오른쪽 아래 왼쪽 위
-dx = [1,0,-1,0]
+dy = [0, 1, 0, -1]  # 오른쪽 아래 왼쪽 위
+dx = [1, 0, -1, 0]
 d = 0
 while True:
-    now = [bam[-1][0]+dy[d%4],bam[-1][1]+dx[d%4]]
+    now = [bam[-1][0]+dy[d % 4], bam[-1][1]+dx[d % 4]]
     if (0 <= now[0] < n) and (0 <= now[1] < n) and (now not in bam):
         bam.append(now)
         time += 1
@@ -37,7 +37,3 @@ while True:
                 d += 3
             del move[0]
 print(time)
-
-
-
-
