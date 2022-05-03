34e801fe47a1e8178f1f57cd71129c6729ecef8b
snowwhitewolf <whdgus1029@naver.com>
2022-05-03 18:43:24 +0900

    0503

--- Baekjoon/0415.py
+++ Baekjoon/0415.py
@@ -1,1 +1,3 @@
-sdf
+N, K = map(int,input().split())
+for _ in range(N-1):
+    W, V = map(int,input().split())
---
+++ Baekjoon/0416.py
@@ -0,0 +1,3 @@
+N, K = map(int,input().split())
+for _ in range(N-1):
+    W, V = map(int,input().split())
