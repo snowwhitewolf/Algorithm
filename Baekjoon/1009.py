7fe57fccc1826a8c6b89071310d4e468515c2b46
snowwhitewolf <whdgus1029@naver.com>
2022-06-01 22:58:12 +0900

    0601

--- Baekjoon/1009.py
+++ Baekjoon/1009.py
@@ -1,4 +1,10 @@
 for t in range(int(input())):
     a,b = map(int,input().split())
-    res = 0
-    print(a,b)
+    res = 1
+    for i in range(b):
+        res *= a%10
+        res = res%10
+    if res == 0:
+        print(10)
+    else:
+        print(res)
